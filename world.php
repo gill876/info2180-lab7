12499810dfdeeb9d99ce77f5b78fc75aa5f53f31
Ubuntu <ubuntu@ip-172-31-89-41.ec2.internal>
2019-11-17 19:52:33 +0000

    Countries showing in very minimalistic style

--- world.php
+++ world.php
@@ -29,21 +29,53 @@ if($_SERVER['REQUEST_METHOD'] === 'GET'){
         $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$santized_country%';");
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
-        <ul>
-        <?php foreach ($results as $row): ?>
-        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-        <?php endforeach; ?>
-        </ul>
+        <table class="countries-table">
+            <thead>
+                <tr>
+                    <th>Country Name</th>
+                    <th>Continent</th>
+                    <th>Independence Year</th>
+                    <th>Head of State</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($results as $row): ?>
+                    <tr>
+                        <td><?= $row['name']?></td>
+                        <td><?= $row['continent']?></td>
+                        <td><?= $row['independence_year']?></td>
+                        <td><?= $row['head_of_state']?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+            <caption>Table showing names of countries including their continent, independence year and their head of state.</caption>
+        </table>
         <?php
     } else{
-        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$santized_country%';");
+        $stmt = $conn->query("SELECT * FROM countries;");
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
-        <ul>
-        <?php foreach ($results as $row): ?>
-        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-        <?php endforeach; ?>
-        </ul>
+        <table class="countries-table">
+            <thead>
+                <tr>
+                    <th>Country Name</th>
+                    <th>Continent</th>
+                    <th>Independence Year</th>
+                    <th>Head of State</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($results as $row): ?>
+                    <tr>
+                        <td><?= $row['name']?></td>
+                        <td><?= $row['continent']?></td>
+                        <td><?= $row['independence_year']?></td>
+                        <td><?= $row['head_of_state']?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+            <caption>Table showing names of countries including their continent, independence year and their head of state.</caption>
+        </table>
         <?php
     }
 }
