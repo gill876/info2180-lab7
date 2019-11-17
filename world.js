33b7d28f17cbc7fe467a8d84b276b23fecf2e9bf
Ubuntu <ubuntu@ip-172-31-89-41.ec2.internal>
2019-11-17 18:57:43 +0000

    Displaying countries in a list

---
+++ world.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    //console.log("hello world");
+    
+    function requestCountry(){
+        let countryI = $("#country").val();
+        countryI = countryI.trim();
+        
+        $.ajax("world.php", {
+            method: 'GET',
+            data: {
+                country: countryI
+            }
+        }).done(function(response){
+            let parseHTML = response;
+            $("#result").html(parseHTML);
+        }).fail(function(){
+            alert('There was an issue with the request.');
+        });
+    }
+    
+    $("#lookup").click(function(){
+        requestCountry();
+    });
+    
+    $("#country").keypress(function(e){
+        if(e.which == 13){
+            requestCountry();
+        }
+    });
+});
--- world.php
+++ world.php
@@ -1,11 +1,18 @@
 <?php
 $host = getenv('IP');
 $username = 'lab7_user';
-$password = '';
+$password = 'qw$:8Kz%';
 $dbname = 'world';
 
+/*
+TABLES:
+cities
+countries
+languages
+*/
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+/*$stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -14,4 +21,29 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+</ul>*/
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    if(!empty($_GET['country'])){
+        $santized_country = filter_var($_GET['country'], FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_FLAG_NO_ENCODE_QUOTES);
+        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$santized_country%';");
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <ul>
+        <?php foreach ($results as $row): ?>
+        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+        <?php endforeach; ?>
+        </ul>
+        <?php
+    } else{
+        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$santized_country%';");
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <ul>
+        <?php foreach ($results as $row): ?>
+        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+        <?php endforeach; ?>
+        </ul>
+        <?php
+    }
+}
